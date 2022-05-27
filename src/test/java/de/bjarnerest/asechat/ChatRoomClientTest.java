2835cde85292b2acc9c7c5e419709039ba0c2aa8
Bjarne Rest <nrest@inovex.de>
2022-05-27 10:29:00 +0200

    Add client leave test

--- src/test/java/de/bjarnerest/asechat/ChatRoomClientTest.java
+++ src/test/java/de/bjarnerest/asechat/ChatRoomClientTest.java
@@ -5,11 +5,14 @@ import static org.awaitility.Awaitility.await;
 import de.bjarnerest.asechat.client.ChatRoomClient;
 import de.bjarnerest.asechat.helper.InstructionNameHelper;
 import de.bjarnerest.asechat.instruction.BaseInstruction;
+import de.bjarnerest.asechat.instruction.ChatLeaveInstruction;
 import de.bjarnerest.asechat.instruction.ChatMessageSendInstruction;
+import de.bjarnerest.asechat.instruction.InstructionInvalidException;
 import de.bjarnerest.asechat.model.Message;
 import de.bjarnerest.asechat.model.Station;
 import de.bjarnerest.asechat.model.User;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PipedInputStream;
@@ -135,12 +138,37 @@ public class ChatRoomClientTest {
 
         String line2 = mockOutputBuffered.readLine();
         BaseInstruction instruction2 = InstructionNameHelper.parseInstruction(line2, Station.CLIENT);
-        assertInstanceOf(ChatMessageSendInstruction.class, instruction);
+        assertInstanceOf(ChatMessageSendInstruction.class, instruction2);
 
         ChatMessageSendInstruction chatMessageSendInstruction2 = (ChatMessageSendInstruction) instruction2;
         assertEquals("Hi!", chatMessageSendInstruction2.getMessage().getMessageText());
         assertEquals("username", chatMessageSendInstruction2.getMessage().getMessageSender().getUsername());
 
+
+    }
+
+    @Test
+    void testLeave() throws IOException, InstructionInvalidException {
+
+        mockInput.write("system:ready\n".getBytes(StandardCharsets.UTF_8));
+
+        // Greeting message
+        await().atMost(Duration.ofSeconds(2)).until(mockOutputBuffered::ready);
+
+        String line = mockOutputBuffered.readLine();
+        BaseInstruction instruction = InstructionNameHelper.parseInstruction(line, Station.CLIENT);
+        assertInstanceOf(ChatMessageSendInstruction.class, instruction);
+
+        ChatMessageSendInstruction chatMessageSendInstruction = (ChatMessageSendInstruction) instruction;
+        assertEquals("Hello Welt. Hier ist username", chatMessageSendInstruction.getMessage().getMessageText());
+        assertEquals("username", chatMessageSendInstruction.getMessage().getMessageSender().getUsername());
+
+        fakeUserOutput.write("/leave\n".getBytes(StandardCharsets.UTF_8));
+        await().atMost(Duration.ofSeconds(2)).until(mockOutputBuffered::ready);
+
+        String line2 = mockOutputBuffered.readLine();
+        BaseInstruction instruction2 = InstructionNameHelper.parseInstruction(line2, Station.CLIENT);
+        assertInstanceOf(ChatLeaveInstruction.class, instruction2);
 
     }
 
